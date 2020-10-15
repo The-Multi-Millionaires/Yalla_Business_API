2ac97f47a482bf91911969fd816ebed5d45d7b8e
Osama-Yousef <osamawalidyousef@gmail.com>
2020-10-15 14:48:41 +0300

    fixing somethings

--- yalla_business_api/settings.py
+++ yalla_business_api/settings.py
@@ -56,8 +56,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
